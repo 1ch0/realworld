fa746b80a884928c40639dcd1368cd4837949e18
1ch0 <github1ch0@163.com>
2022-02-08 14:47:38 +0800

    feat: add db

---
+++ data/data.go
@@ -0,0 +1,40 @@
+package data
+
+import (
+	"realworld/internal/conf"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// ProviderSet is data providers.
+var ProviderSet = wire.NewSet(NewData, NewDB, NewGreeterRepo)
+
+// Data .
+type Data struct {
+	db *gorm.DB
+}
+
+// NewData .
+func NewData(c *conf.Data, logger log.Logger, db *gorm.DB) (*Data, func(), error) {
+	cleanup := func() {
+		log.NewHelper(logger).Info("closing the data resources")
+	}
+	return &Data{db: db}, cleanup, nil
+}
+
+func NewDB(c *conf.Data) *gorm.DB {
+	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(c.Database.dsn), &gorm.Config{})
+	if err != nil {
+		s
+		panic("failed to connect database")
+	}
+	if err := db.AutoMigrate(); err != nil {
+		panic(err)
+	}
+	return db
+}
